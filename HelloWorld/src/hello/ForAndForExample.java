21587921b9b6f3d4f6e052b992171153303d7c11
User <User@YD03-09>
2019-09-16 14:58:21 +0900

    commit

---
+++ HelloWorld/src/hello/ForAndForExample.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class ForAndForExample {
+	public static void main(String[] args) {
+		for (int i=0; i<10; i++) {
+			for (int j=0; j<10; j++) {
+				System.out.println("j>> "+j);
+			}
+			System.out.println("i:: "+i);
+		}
+	}
+
+}
